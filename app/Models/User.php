f6f821a12b3dd471ca9266b67d9bdc6e051a4e83
Oleksiy Hr <alexey.gritsaiy@gmail.com>
2022-05-28 10:15:46 +0300

    create register and send mail to user email with token

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,33 +12,30 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    const STATUS_ACTIVE = 'active';
+    const STATUS_WAIT = 'wait';
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'status',
+        'token',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function verify()
+    {
+        $this->update([
+            'status' => self::STATUS_ACTIVE,
+        ]);
+    }
 }
